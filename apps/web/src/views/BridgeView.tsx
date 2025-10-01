280e206c2b70488d684e7a785803445ce55a4dcb
chef-ryan <ryan@pancakeswap.com>
2025-10-02 01:14:22 +0800

    fix: remove build warning (#12412)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on enhancing the `BridgeView.tsx` component by
    implementing dynamic imports for the `CanonicalBridge` to improve
    performance and reduce import trace warnings. Additionally, it updates
    the environment variables in the `turbo.json` configuration.
    
    ### Detailed summary
    - Added `.envrc` file.
    - Implemented dynamic import for `CanonicalBridge` in `BridgeView.tsx`.
    - Updated environment variables in `turbo.json` to include several new
    keys such as `CRON_SECRET`, `R2_ENDPOINT`, and `DISCORD_CLIENT_SECRET`.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/BridgeView.tsx
+++ apps/web/src/views/BridgeView.tsx
@@ -1,14 +1,21 @@
-import { CanonicalBridge } from '@pancakeswap/canonical-bridge'
 import { ChainId, chainNames, NonEVMChainId } from '@pancakeswap/chains'
 import { Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 import { PUBLIC_NODES } from 'config/nodes'
+import dynamic from 'next/dynamic'
 import { Suspense, useEffect, useMemo } from 'react'
 import { CHAIN_IDS } from 'utils/wagmi'
 import Page from 'views/Page'
 import { useSwitchNetwork } from 'hooks/useSwitchNetwork'
 
 const DISABLED_TO_CHAINS: ChainId[] = []
+// Dynamic import to avoid import trace warnings
+const CanonicalBridge = dynamic(
+  () => import('@pancakeswap/canonical-bridge').then((mod) => ({ default: mod.CanonicalBridge })),
+  {
+    ssr: false,
+  },
+)
 
 // Fix portal conflicts between Privy and Chakra portals
 function usePortalConflictFix() {
